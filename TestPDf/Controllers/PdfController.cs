1da2df890404d6930cd13da4d7c742ee21c5ca9f
Asamy2000 <samy07002@gmail.com>
2024-06-09 14:45:22 +0300

    Initial

---
+++ TestPDf/Controllers/PdfController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestPDf.Helpers;
+
+namespace TestPDf.Controllers
+{
+    public class PdfController : Controller
+    {
+        private readonly PdfMergeService _pdfMergeService;
+
+        public PdfController(PdfMergeService pdfMergeService)
+        {
+            _pdfMergeService = pdfMergeService;
+        }
+
+        [HttpGet]
+        public IActionResult Merge()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Merge(List<IFormFile> files)
+        {
+            if (files == null || files.Count == 0)
+            {
+                ViewBag.Message = "Please select at least one file.";
+                return View();
+            }
+
+            var filePaths = new List<string>();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.Combine(Path.GetTempPath(), formFile.FileName);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        formFile.CopyTo(stream);
+                    }
+                    filePaths.Add(filePath);
+                }
+            }
+
+            var mergedFileBytes = _pdfMergeService.MergeFiles(filePaths);
+            var mergedFileStream = new MemoryStream(mergedFileBytes);
+
+            return File(mergedFileStream, "application/pdf", "merged.pdf");
+        }
+    }
+}
---
+++ TestPDf/Helpers/PdfMergeService.cs
@@ -0,0 +1,74 @@
+﻿using iText.IO.Image;
+using iText.Kernel.Pdf;
+using iText.Kernel.Utils;
+using iText.Layout;
+using iText.Layout.Element;
+
+namespace TestPDf.Helpers
+{
+
+    //public class PdfMergeService
+    //{
+    //    public byte[] MergePdfFiles(List<string> pdfFiles)
+    //    {
+    //        using (var memoryStream = new MemoryStream())
+    //        {
+    //            using (var writer = new PdfWriter(memoryStream))
+    //            {
+    //                using (var pdfDocument = new PdfDocument(writer))
+    //                {
+    //                    PdfMerger pdfMerger = new PdfMerger(pdfDocument);
+    //                    foreach (var file in pdfFiles)
+    //                    {
+    //                        using (var sourcePdf = new PdfDocument(new PdfReader(file)))
+    //                        {
+    //                            pdfMerger.Merge(sourcePdf, 1, sourcePdf.GetNumberOfPages());
+    //                        }
+    //                    }
+    //                }
+    //            }
+    //            return memoryStream.ToArray();
+    //        }
+    //    }
+    //}
+
+
+
+    public class PdfMergeService
+    {
+        public byte[] MergeFiles(List<string> filePaths)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var writer = new PdfWriter(memoryStream))
+                {
+                    using (var pdfDocument = new PdfDocument(writer))
+                    {
+                        var document = new Document(pdfDocument);
+                        PdfMerger pdfMerger = new PdfMerger(pdfDocument);
+
+                        foreach (var filePath in filePaths)
+                        {
+                            var extension = Path.GetExtension(filePath).ToLower();
+
+                            if (extension == ".pdf")
+                            {
+                                using (var sourcePdf = new PdfDocument(new PdfReader(filePath)))
+                                {
+                                    pdfMerger.Merge(sourcePdf, 1, sourcePdf.GetNumberOfPages());
+                                }
+                            }
+                            else if (extension == ".jpg" || extension == ".jpeg" || extension == ".png")
+                            {
+                                ImageData imageData = ImageDataFactory.Create(filePath);
+                                var image = new Image(imageData);
+                                document.Add(image);
+                            }
+                        }
+                    }
+                }
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
